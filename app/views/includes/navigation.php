5a17a95e3e30d906b09832470e3a42665788494a
Guilherme Lopes <guilherme-lazarini@hotmail.com>
2020-08-24 19:55:58 -0300

    Update navigation.php

---
+++ app/views/includes/navigation.php
@@ -0,0 +1,19 @@
+<nav class="top-nav">
+    <ul>
+        <li>
+            <a href="<?php echo URLROOT; ?>/pages/index">Home</a>
+        </li>
+        <li>
+            <a href="<?php echo URLROOT; ?>/pages/about">Sobre</a>
+        </li>
+        <li>
+            <a href="<?php echo URLROOT; ?>/pages/projects">Projetos</a>
+        </li>
+        <li>
+            <a href="<?php echo URLROOT; ?>/pages/contact">Contato</a>
+        </li>
+        <li class="btn-login">
+            <a href="<?php echo URLROOT; ?>/users/login">Login</a>
+        </li>
+    </ul>
+</nav>
