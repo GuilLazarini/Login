e8853ddbb46af8cec5dfe4a8faeaef8c24dae64e
Guilherme Lopes <guilherme-lazarini@hotmail.com>
2020-08-24 20:09:28 -0300

    https://github.com/GuilLazarini/Login.git

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -2,8 +2,13 @@
     require APPROOT . '/views/includes/head.php';
 ?>
 
-<div id="section-landing">
+<div id="containers">
     <?php
         require APPROOT . '/views/includes/navigation.php';
     ?>
+
+    <div class="banner">
+        <h1>Lorem ipsum dolor sit amet</h1>
+        <h2>consectetur adipiscing elit</h2>
+    </div>
 </div>
